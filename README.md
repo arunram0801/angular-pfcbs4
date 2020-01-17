3fa462746eddccc686bbf399fdedf1e61acf6726
arunram0801 <arunram0801@gmail.com>
2020-01-17 11:57:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-pfcbs4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-pfcbs4)
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[]=[
+    new Recipe("Recipe #1","Test Recipe","https://drop.ndtv.com/albums/COOKS/pasta-vegetarian/pastaveg_640x480.jpg"),
+    new Recipe("Recipe #2","Another Test Recipe","https://drop.ndtv.com/albums/COOKS/pasta-vegetarian/pastaveg_640x480.jpg")
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
